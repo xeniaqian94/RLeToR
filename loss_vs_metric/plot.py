9aa11a75197a4c111ea05d3e4ba7da90587d5d99
Xin Qian <xeniaqian94@gmail.com>
2017-04-07 15:24:13 -0400

    Update

--- loss_vs_metric/plot.py
+++ loss_vs_metric/plot.py
@@ -2,39 +2,33 @@ import sys
 import re
 from collections import defaultdict
 import matplotlib.pyplot as plt
+import numpy as np
 
-f=open(sys.argv[1],"r")
+f = open(sys.argv[1], "r")
 
-training_process_loss=defaultdict(lambda:dict())
-training_process_ndcg=defaultdict(lambda:dict())
+training_process_loss = defaultdict(lambda: dict())
+training_process_ndcg = defaultdict(lambda: dict())
 
 for line in f.readlines():
-    process=int(re.search(r".condor_out.([0-9]+).", line).group(1))
-    run=int(re.search(r"repeated train \[([0-9]+)\]", line).group(1))
-    loss=float(re.search(r"with loss \[([.0-9_]+)\]", line).group(1))
-    ndcg=float(re.search(r"train ndcg \[([.0-9_]+)\]", line).group(1))
-    training_process_loss[process][run]=loss
-    training_process_ndcg[process][run]=ndcg
-
-# f, axarr = plt.subplots(len(training_process_ndcg.keys()), sharex=True,figsize=(8, 40))
-#
-#
-# for subi in range(len(training_process_ndcg.keys())):
-#     process=list(training_process_ndcg.keys())[subi]
-#
-#     axarr[subi].plot(list(training_process_loss[process].keys()),list(training_process_loss[process].values()),'r--',list(training_process_ndcg[process].keys()),list(training_process_ndcg[process].values()),'g.')
-#     axarr[subi].set_title(process)
-#     axarr[subi].grid(True)
-#
-#
-# f.savefig('full_figure.png')
+    process = int(re.search(r".condor_out.([0-9]+).", line).group(1))
+    run = int(re.search(r"repeated train \[([0-9]+)\]", line).group(1))
+    loss = 1 - float(re.search(r"with loss \[([.0-9_]+)\]", line).group(1))
+    ndcg = float(re.search(r"train ndcg \[([.0-9_]+)\]", line).group(1))
+    training_process_loss[process][run] = loss
+    training_process_ndcg[process][run] = ndcg
 
 for subi in range(len(training_process_ndcg.keys())):
     process = list(training_process_ndcg.keys())[subi]
     plt.clf()
-    plt.plot(list(training_process_loss[process].keys()),list(training_process_loss[process].values()),'r--',label='loss')
-    plt.plot(list(training_process_ndcg[process].keys()),list(training_process_ndcg[process].values()),'g--',label='nDCG(training)')
+    plt.plot(list(training_process_loss[process].keys()), list(training_process_loss[process].values()), 'r--',
+             label='1-loss')
+    plt.plot(list(training_process_ndcg[process].keys()), list(training_process_ndcg[process].values()), 'g--',
+             label='nDCG(training)')
     plt.title(process)
+    plt.xlabel("repeated train #")
+    plt.ylabel("1-loss or nDCG #")
     plt.legend(loc='upper left')
     plt.grid(True)
-    plt.savefig(sys.argv[1]+"_plots/"+str(process)+".png")
+    plt.savefig(sys.argv[1] + "_plots/" + str(process) + ".png")
+
+print(np.mean([len(training_process_loss[process]) for process in training_process_loss]))
